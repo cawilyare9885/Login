1e77089b24f9b4ed5bc07703936dbf616d38f949
Awil M. Yusuf <63840466+cawilyare9885@users.noreply.github.com>
2020-10-29 14:14:45 +0300

    Update Login.java
    waa comment wax dhib leh maaha sb

--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -17,6 +17,7 @@ public class Login {
     public static void main(String[] args) {
         // TODO code application logic here
         new Signup().setVisible(true);
+        //ninkii waynaa waxaas buu ku daray codekii
     }
     
 }
